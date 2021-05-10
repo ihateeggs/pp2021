6d8b06ae0ada4e1df87acba1d8c6d15a6ad16a6a
ihateeggs <daikia17@gmail.com>
2021-05-11 02:44:21 +0700

    Add zipfile

--- pw5/main.py
+++ pw5/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import zipfile
 
 from inputPacket import input_students_info
 std_num = input_students_info.input_Student_numb()
@@ -15,3 +16,7 @@ listc = list_info.list_Course(course_info)
 from markPacket import input_Mark
 mark = input_Mark.input_Marks(course_info,std_info)
 
+with zipfile.ZipFile('students.dat', 'w', compression=zipfile.ZIP_DEFLATED) as myzip:
+    myzip.write("student.txt")
+    myzip.write("courses.txt")
+    myzip.write("marks.txt")
